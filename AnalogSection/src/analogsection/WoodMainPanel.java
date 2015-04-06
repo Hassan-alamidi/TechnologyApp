a78b11d55e3bff8bf4df9347697eef62ec93f8c4
stephenkearns1 <Sean@Sean-Murrays-MacBook-Pro.local>
2015-04-06 02:42:07 +0100

    paddys section

--- AnalogSection/src/analogsection/WoodMainPanel.java
+++ AnalogSection/src/analogsection/WoodMainPanel.java
@@ -32,24 +32,18 @@ public class WoodMainPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jProgressBar1 = new javax.swing.JProgressBar();
-        InfoLbl = new javax.swing.JLabel();
-        QuizLbl = new javax.swing.JLabel();
-        WhatsLbl = new javax.swing.JLabel();
         ExitBtn = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        QuizBtn = new javax.swing.JButton();
+        QuizLbl = new javax.swing.JLabel();
+        InfoLbl1 = new javax.swing.JLabel();
+        WhatsLbl = new javax.swing.JLabel();
         WhatsBtn = new javax.swing.JButton();
-        HomeBtn = new javax.swing.JButton();
-        InfoBtn = new javax.swing.JButton();
+        QuizBtn = new javax.swing.JButton();
         BackgroundLbl = new javax.swing.JLabel();
+        InfoBtn = new javax.swing.JButton();
+        HomeBtn = new javax.swing.JButton();
 
         setLayout(null);
-        add(InfoLbl);
-        InfoLbl.setBounds(110, 170, 140, 0);
-        add(QuizLbl);
-        QuizLbl.setBounds(120, 180, 140, 60);
-        add(WhatsLbl);
-        WhatsLbl.setBounds(120, 250, 160, 70);
 
         ExitBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/analogsection/exit.png"))); // NOI18N
         ExitBtn.addActionListener(new java.awt.event.ActionListener() {
@@ -58,21 +52,23 @@ public class WoodMainPanel extends javax.swing.JPanel {
             }
         });
         add(ExitBtn);
-        ExitBtn.setBounds(330, 10, 40, 45);
+        ExitBtn.setBounds(330, 10, 40, 41);
 
-        jLabel2.setText("jLabel2");
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\John\\Desktop\\rsz_mainmenu2 - Copy.gif")); // NOI18N
         add(jLabel2);
-        jLabel2.setBounds(90, 340, 160, 30);
+        jLabel2.setBounds(100, 340, 170, 30);
 
-        QuizBtn.setBackground(new java.awt.Color(255, 255, 255));
-        QuizBtn.setText("To image Quiz");
-        QuizBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                QuizBtnActionPerformed(evt);
-            }
-        });
-        add(QuizBtn);
-        QuizBtn.setBounds(120, 180, 140, 60);
+        QuizLbl.setIcon(new javax.swing.ImageIcon("C:\\Users\\John\\Desktop\\rsz_1rsz_1rsz_quiz.jpg")); // NOI18N
+        add(QuizLbl);
+        QuizLbl.setBounds(120, 170, 140, 60);
+
+        InfoLbl1.setIcon(new javax.swing.ImageIcon("C:\\Users\\John\\Desktop\\rsz_1rsz_1imageedit_4_4600978165.png")); // NOI18N
+        add(InfoLbl1);
+        InfoLbl1.setBounds(110, 100, 140, 60);
+
+        WhatsLbl.setIcon(new javax.swing.ImageIcon("C:\\Users\\John\\Desktop\\rsz_title859716636.png")); // NOI18N
+        add(WhatsLbl);
+        WhatsLbl.setBounds(120, 250, 160, 70);
 
         WhatsBtn.setBackground(new java.awt.Color(255, 255, 255));
         WhatsBtn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
@@ -85,15 +81,19 @@ public class WoodMainPanel extends javax.swing.JPanel {
         add(WhatsBtn);
         WhatsBtn.setBounds(120, 250, 160, 70);
 
-        HomeBtn.setBackground(new java.awt.Color(0, 102, 204));
-        HomeBtn.setText("Previous Menu");
-        HomeBtn.addActionListener(new java.awt.event.ActionListener() {
+        QuizBtn.setBackground(new java.awt.Color(255, 255, 255));
+        QuizBtn.setText("To image Quiz");
+        QuizBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                HomeBtnActionPerformed(evt);
+                QuizBtnActionPerformed(evt);
             }
         });
-        add(HomeBtn);
-        HomeBtn.setBounds(100, 340, 150, 30);
+        add(QuizBtn);
+        QuizBtn.setBounds(120, 170, 140, 60);
+
+        BackgroundLbl.setIcon(new javax.swing.ImageIcon("C:\\Users\\John\\Desktop\\paddysimg.jpg.jpg")); // NOI18N
+        add(BackgroundLbl);
+        BackgroundLbl.setBounds(0, -10, 400, 470);
 
         InfoBtn.setBackground(new java.awt.Color(255, 255, 255));
         InfoBtn.setText("To Wood Info");
@@ -104,8 +104,16 @@ public class WoodMainPanel extends javax.swing.JPanel {
         });
         add(InfoBtn);
         InfoBtn.setBounds(110, 100, 120, 50);
-        add(BackgroundLbl);
-        BackgroundLbl.setBounds(0, 0, 400, 440);
+
+        HomeBtn.setBackground(new java.awt.Color(0, 102, 204));
+        HomeBtn.setText("Previous Menu");
+        HomeBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                HomeBtnActionPerformed(evt);
+            }
+        });
+        add(HomeBtn);
+        HomeBtn.setBounds(100, 340, 150, 30);
     }// </editor-fold>//GEN-END:initComponents
 
     private void InfoBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InfoBtnActionPerformed
@@ -119,19 +127,6 @@ public class WoodMainPanel extends javax.swing.JPanel {
         }
 // TODO add your handling code here:
     }//GEN-LAST:event_InfoBtnActionPerformed
-
-    private void QuizBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_QuizBtnActionPerformed
-            LayoutManager layout = getParent().getLayout();
-        if (layout instanceof java.awt.CardLayout) {
-            java.awt.CardLayout cl = (java.awt.CardLayout)layout;
-            cl.show(getParent(), "WoodQuizScreen");
-        }
-          
-          
-
-
-// TODO add your handling code here:
-    }//GEN-LAST:event_QuizBtnActionPerformed
 
     private void WhatsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_WhatsBtnActionPerformed
         LayoutManager layout = getParent().getLayout();
@@ -157,13 +152,26 @@ public class WoodMainPanel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_HomeBtnActionPerformed
 
+    private void QuizBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_QuizBtnActionPerformed
+        WoodQuiz wood = new WoodQuiz();
+
+        wood.choseRandomQuestion();
+        LayoutManager layout = getParent().getLayout();
+        if (layout instanceof java.awt.CardLayout) {
+            java.awt.CardLayout cl = (java.awt.CardLayout)layout;
+            cl.show(getParent(), "QuizScreen");
+        }
+
+        // TODO add your handling code here:
+    }//GEN-LAST:event_QuizBtnActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel BackgroundLbl;
     private javax.swing.JButton ExitBtn;
     private javax.swing.JButton HomeBtn;
     private javax.swing.JButton InfoBtn;
-    private javax.swing.JLabel InfoLbl;
+    private javax.swing.JLabel InfoLbl1;
     private javax.swing.JButton QuizBtn;
     private javax.swing.JLabel QuizLbl;
     private javax.swing.JButton WhatsBtn;
